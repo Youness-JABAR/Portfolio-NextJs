0cf5b3ed1766f5382d759fe2d445866b7d35b5c1
Youness-JABAR <youness.jabar.pro@gmail.com>
2023-08-09 22:30:20 +0100

    feat: add opengraph and twitter metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,11 +11,33 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-
   title: 'Youness JABAR Portfolio',
   description: "I'm a web Developer And Ai lover, Worked on various types of projects and I'm confident you'll enjoy working with me.",
   icons: {
     icon: '/favicon.png',
+  },
+  openGraph: {
+    title: 'Youness JABAR Portfolio',
+    description: "I'm a web Developer And Ai lover, Worked on various types of projects and I'm confident you'll enjoy working with me.",
+    url: 'https://youness-jabar.vercel.app/',
+    siteName: 'youness-jabar',
+    images: [
+      {
+        url: '/favicon.png',
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Youness JABAR Portfolio',
+    description: "I'm a web Developer And Ai lover, Worked on various types of projects and I'm confident you'll enjoy working with me.",
+    site: '@youness-jabar', 
+    // creator: '@yourusername', 
+    images: ['/favicon.png'],
   },
 
 }
